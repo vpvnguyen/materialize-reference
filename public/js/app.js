f61496dd3501c7f1885ed4a7d5d5b3e13596f3f2
Vincent Nguyen <vpvnguyen@gmail.com>
2019-09-29 11:10:11 -0700

    init materialize

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+// floating action button
+$(document).ready(function () {
+    $('.fixed-action-btn').floatingActionButton();
+});
+
+// collapsible elements
+$(document).ready(function () {
+    $('.collapsible').collapsible();
+});
+
+// feature discovery
+$(document).ready(function () {
+    $('.tap-target').tapTarget();
+    $('.tap-target').tapTarget('open')
+});
+
+// tabs
+$(document).ready(function () {
+    $('.tabs').tabs();
+});
+
